232f33240fa524c03e7b23f380adcffdee4748ce
Luiz Gouveia <lgou90@gmail.com>
2024-06-24 19:36:06 -0300

    Update wsgi.py for debugging paths

--- yt_transcription/yt_transcription/wsgi.py
+++ yt_transcription/yt_transcription/wsgi.py
@@ -1,5 +1,9 @@
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
+
+print("Current working directory:", os.getcwd(), file=sys.stderr)
+print("Python path:", sys.path, file=sys.stderr)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yt_transcription.settings')
 application = get_wsgi_application()
