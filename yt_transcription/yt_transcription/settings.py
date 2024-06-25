28aa5be74b7e747ed0ba6df758a861174432d2b3
Luiz Gouveia <lgou90@gmail.com>
2024-06-24 22:09:48 -0300

    remove migrations folder

--- yt_transcription/yt_transcription/settings.py
+++ yt_transcription/yt_transcription/settings.py
@@ -64,7 +64,10 @@ WSGI_APPLICATION = 'yt_transcription.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
