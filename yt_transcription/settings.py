5dda1fa311c36495626ce801af3b0daf5bdab182
Luiz Gouveia <lcpgou@gmail.com>
2024-08-28 09:59:49 -0300

    Updated settings for Heroku deployment

--- yt_transcription/settings.py
+++ yt_transcription/settings.py
@@ -1,8 +1,7 @@
 import os
-import dj_database_url
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
@@ -80,10 +79,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'yt_transcription.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 CACHES = {
